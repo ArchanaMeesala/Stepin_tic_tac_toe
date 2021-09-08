d5bb72ba5e65d978a545b982d75cf3f19c3591ea
ArchanaMeesala <77663747+ArchanaMeesala@users.noreply.github.com>
2021-09-08 17:35:38 +0530

    commit

--- 3_Implementations/test/test.c
+++ 3_Implementations/test/test.c
@@ -16,18 +16,18 @@ void tearDown()
 
 void test_successfullWin()
 {
-    char arr[10]={'0','x','x','x','o','x','o','o','o','x'};
-     TEST_ASSERT_EQUAL(1, checkWinner());
+     
+     TEST_ASSERT_EQUAL(1, markplay());
 }
 void test_successfuldraw()
 {
-    char arr[10]={'0','o','x','o','o','x','o','x','o','x'};
-    TEST_ASSERT_EQUAL(0,checkWinner());
+    
+    TEST_ASSERT_EQUAL(0,markplay());
 }
 void test_Emptyboard()
 {
-    char arr[10]={'0','1','2','3','4','5','6','7','8','9'};
-    TEST_ASSERT_EQUAL(-1,checkWinner());
+  
+    TEST_ASSERT_EQUAL(-1,markplay());
 }
 int main()
 {
